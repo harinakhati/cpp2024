c23863152b004ad4d1077f531ac71483dc8aba70
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-06-08 08:54:16 +0545

    c6.cpp c6.exe

---
+++ c6.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+bool Prime(int n )
+{
+		if(n == 1 || n == 0)
+         return false;
+
+for(int i = 2; i < n; i++)
+	{
+		if(n % i == 0)
+         return false;
+	}
+		return true;
+}
+int main()
+{
+    int n1,n2;
+    cout << "Enter the first number: ";
+    cin >> n1;
+    cout << "Enter the second number: ";
+    cin >> n2;
+   for(int i=n1;i<=n2;i++)
+   {
+{
+				if(Prime(i)) 
+		{
+			cout << i << " ";
+		}
+	}
+   }
+	return 0;
+}
