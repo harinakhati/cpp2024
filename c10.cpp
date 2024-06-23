8f0726b45756e41fcbefd9bc49172e007441e1ff
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-06-23 21:25:53 +0545

    c10.cpp c10.exe

---
+++ c10.cpp
@@ -0,0 +1,54 @@
+//Write a program in C++ to find product of two 3*3 matrices
+#include<iostream>
+using namespace std;
+
+void multiplyMatrices(int mat1[3][3], int mat2[3][3], int prod[3][3]) {
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            prod[i][j] = 0;
+            for(int k = 0; k < 3; k++) {
+                prod[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+}
+
+void printMatrix(int mat[3][3]) {
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int mat1[3][3], mat2[3][3], res[3][3];
+
+    cout << "Enter elements of first matrix:" << endl;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> mat1[i][j];
+        }
+    }
+
+    cout << "Enter elements of second matrix:" << endl;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> mat2[i][j];
+        }
+    }
+
+    multiplyMatrices(mat1, mat2, res);
+
+    cout << "Matrix 1:" << endl;
+    printMatrix(mat1);
+
+    cout << "Matrix 2:" << endl;
+    printMatrix(mat2);
+
+    cout << "Product of two matrices:" << endl;
+    printMatrix(res);
+
+    return 0;
+}
