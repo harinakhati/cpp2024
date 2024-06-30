e5e158c29f1401bb59e954ccd9443efc969e99b4
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-06-30 17:46:35 +0545

    c16.cpp c16.exe .vscode/launch.json

---
+++ c16.cpp
@@ -0,0 +1,27 @@
+/*Write a function called zeroSmaller () that is passed two int arguments by reference 
+and then sets the smaller of two numbers to 0. Write a main () program to exercise this function.*/
+#include <iostream>
+using namespace std;
+void zeroSmaller(int &a, int &b)
+{
+    if (a < b)
+    {
+        a= 0;
+        }
+        else
+        {
+            b = 0;
+            }
+            }
+            int main()
+            {
+                int a, b;
+                cout << "Enter two numbers: ";
+                cin >> a >> b;
+                zeroSmaller(a, b);
+                cout << "//The smaller number is: " << "0"<< endl;
+                cout << "The number is: " << a << endl;
+                cout << "The number is: " << b << endl;
+                return 0;
+            }
+
