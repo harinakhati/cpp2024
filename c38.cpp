f57f609a77387b6d9a4d82019c57efd4d729fd2f
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-07-12 14:21:29 +0545

    c38.cpp c38.exe

---
+++ c38.cpp
@@ -0,0 +1,44 @@
+/*Write a program in C++ that finds difference of two distances 
+(feet, inch) by overloading binary operator -*/
+#include<iostream>
+using namespace std;
+class Distance
+{
+    private:
+    int feet,inch;
+    public:
+    Distance()
+    {
+        feet=inch=0;
+        }
+        Distance(int f,int i)
+        {
+            feet=f;
+            inch=i;
+            }
+            void display()
+            {
+                cout<<"feet="<<feet<<endl<<"inch="<<inch<<endl;
+                }
+                Distance operator -(Distance d)
+                {
+                    Distance temp;
+                    temp.feet=feet-d.feet;
+                    temp.inch=inch-d.inch;
+                    if(temp.inch>=12)
+                    {
+                        temp.feet+=temp.inch/12;
+                        temp.inch=temp.inch%12;
+                        }
+                        return temp;
+                        }
+};
+                        int main()
+                        {
+                            Distance d1(10,11);
+                            Distance d2(20,21);
+                            Distance d3;
+                             d3=d1-d2;
+                            d3.display();
+                            return 0;
+                        }
