6ff0c96b605f85735bbbe9b16e9b085a0d71b2e9
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-07-12 15:09:48 +0545

    c36.cpp c36.exe c43.cpp c43.exe

--- c43.cpp
+++ c43.cpp
@@ -1,4 +1,5 @@
-/*Write a program in C++ that finds product two complex numbers by overloading binary operator * */
+/*Write a program in C++ that finds product two complex numbers by overloading 
+binary operator *  using friend function*/
 #include <iostream>
 using namespace std;
 class complex
@@ -19,14 +20,15 @@ class complex
         {
             cout<<"The complex number is "<<real<<" + "<<img<<"i"<<endl;
             }
-            complex operator *(complex c)
+          friend complex operator *(complex c1,complex c2);
+          };
+          complex operator *(complex c1,complex c2)
             {
                 complex temp;
-                temp.real=real*c.real+img*c.img;
-                temp.img=real*c.img+img*c.real;
+                temp.real=c1.real*c2.real+c1.img*c2.img;
+                temp.img=c1.real*c2.img+c1.img*c2.real;
                 return temp;
                 }
-};
 int main()
 {
     complex c1(3,4);
