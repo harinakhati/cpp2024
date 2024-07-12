0f5a5c0530e10a7b522c65029789a83e96563983
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-07-12 15:03:08 +0545

    c42.cpp c42.exe

---
+++ c42.cpp
@@ -0,0 +1,40 @@
+/*Write a program in C++ that add two complex numbers by 
+overloading binary operator + using friend function*/
+#include <iostream>
+using namespace std;
+class complex
+{
+    private:
+    int real,img;
+    public:
+    complex()
+    {
+        real=img=0;
+    }
+    complex(int r,int i)
+    {
+        real=r;
+        img=i;
+        }
+        void display()
+        {
+            cout<<"The complex number is "<<real<<" + "<<img<<"i"<<endl;
+            }
+          friend complex operator +(complex c1,complex c2);
+          };
+          complex operator +(complex c1,complex c2)
+            {
+                complex temp;
+                temp.real=c1.real+c2.real;
+                temp.img=c1.img+c2.img;
+                return temp;
+                }
+int main()
+{
+    complex c1(3,4);
+    complex c2(5,6);
+    complex c3;
+    c3=c1+c2;
+    c3.display();
+    return 0;
+}
