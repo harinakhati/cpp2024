f0d19020cb2dd8ca304f6fe0c36c65daec4e4fe5
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-07-12 13:11:26 +0545

    c31.cpp c31.exe

---
+++ c31.cpp
@@ -0,0 +1,39 @@
+/*Write a program in C++ that overloads unary operator ++*/
+#include<iostream>
+using namespace std;
+class Space
+{
+    private:
+    int x,y,z;
+    public:
+    Space()
+    {
+        x=y=z=0;
+        }
+        Space(int a,int b,int c)
+        {
+            x=a;
+            y=b;
+            z=c;
+            }
+            void display()
+            {
+                cout<<"("<<x<<","<<y<<","<<z<<")"<<endl;
+                }
+                void operator ++(int)
+                {
+                   x++;
+                   y++;
+                   z++;
+                    }
+};
+int main()
+{
+    Space s(1,2,3);
+    s++;
+    s.display();
+    return 0;
+}
+    
+
+
---
+++ c32.cpp
@@ -0,0 +1,2 @@
+/*Write a program in C++ that overloads unary operator - -*/
+#include<iostream>
