0959f17b93ca1cdefa60f07a12de2e101af33d84
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-07-12 10:37:56 +0545

    c24.cpp c24.exe .vscode/launch.json

--- c24.cpp
+++ c24.cpp
@@ -8,3 +8,70 @@ The display should in the format of feet and inches or meters and centimeters
 depending on the object on display.*/
 #include<iostream>
 using namespace std;
+class DM
+{
+    private:
+       int m,c;
+       public:
+       void getdata()
+       {
+        cout<<"Enter the value of meters and centimeters"<<endl;
+        cin>>m>>c;
+        }
+        void display()
+        {
+            cout<<"The value of meters and centimeters is "<<m<<"m "<<c<<"cm"<<
+            endl;
+            }
+            friend DM operator+(DM a,DM b);
+            };
+            class DB
+            {
+                private:
+                int f,i;
+                public:
+                void getdata()
+                {
+                    cout<<"Enter the value of feet and inches"<<endl;
+                    cin>>f>>i;
+                    }
+                    void display()
+                    {
+                        cout<<"The value of feet and inches is "<<f<<"ft "<<i<<"in"<<
+                        endl;
+                        }
+                        friend DB operator+(DB a,DB b);
+                        };
+                        DB operator+(DB a,DB b)
+                        {
+                            DB c;
+                            c.f=a.f+b.f;
+                            c.i=a.i+b.i;
+                            if(c.i>=12){
+                            	c.f++;
+                            	c.i-=12;
+							}
+                            return c;
+                            }
+                            DM operator+(DM a,DM b)
+                            {
+                                DM c;
+                                c.m=a.m+b.m;
+                                c.c=a.c+b.c;
+                                if(c.c>=100)
+                                {
+                                	c.m++;
+                                	c.c-=100;
+								}
+                                return c;
+                            }
+                            int main()
+                            {
+                                DM d1,d2,d3;
+                                DB d4,d5,d6;
+                                d3=d1+d2;
+                                d6=d4+d5;
+                                d3.display();
+                                d6.display();
+                                return 0;
+                            }
