63935e17d4d38e6914e2a71b5b4b7dc89276de40
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-07-12 09:56:23 +0545

    c23.exe c24.cpp

---
+++ c24.cpp
@@ -0,0 +1,10 @@
+/*Create two classes DM and DB which store the value of distances.
+DM stores distances in meters and centimeters and DB in feet and inches.
+Write a program that can read values for the class objects and add one 
+object of DM with another object of DM. Use friend function to carry out
+the addition operation. The object that stores the results may be a DM object 
+or DB object, depending on the units in which the results are required. 
+The display should in the format of feet and inches or meters and centimeters
+depending on the object on display.*/
+#include<iostream>
+using namespace std;
