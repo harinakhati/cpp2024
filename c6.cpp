d5e61d872d6e9cd5e78b86c10a58f7556f3e939b
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-08-03 13:49:44 +0545

    c6.cpp c6.exe c7.exe c8.exe

--- c6.cpp
+++ c6.cpp
@@ -19,9 +19,13 @@ int main()
     cin >> n1;
     cout << "Enter the second number: ";
     cin >> n2;
+    if (n1 > n2) {
+        cout << "Error: n1 should be less than n2." << endl;
+        return 1;
+    }
    for(int i=n1;i<=n2;i++)
    {
-{
+   {
 				if(Prime(i)) 
 		{
 			cout << i << " ";
